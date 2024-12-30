ea651d5fafc6330fdc35ca2f02d8aa297ec645da
aleksgov <aleksandrgovo@gmail.com>
2024-12-30 12:34:16 +0300

    app rebuild

--- src/App.js
+++ src/App.js
@@ -1,67 +1,73 @@
 import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 import { labFiles } from './Globals';
-import Accordion from './Accordion'; // Импорт компонента аккордеона
+import Accordion from './Accordion';
 
 function App() {
     const taskContentRef = useRef(null);
-    const [forceUpdate, setForceUpdate] = useState();
+    const [forceUpdate, setForceUpdate] = useState(0);
     const [activeTab, setActiveTab] = useState(0);
-    const [tabs, setTabs] = useState([{ title: 'Главная', content: 'Контент главной страницы' },]);
+    const [tabs, setTabs] = useState([{ title: 'Главная', content: 'Контент главной страницы' }]);
     const [theoryContent, setTheoryContent] = useState('');
     const [taskContent, setTaskContent] = useState('');
-    const [labNumber, setLabNumber] = useState(null); // Состояние для хранения номера лабораторной работы
+    const [labNumber, setLabNumber] = useState(null);
     const [activeVariant, setActiveVariant] = useState(1);
     const [variantsCount, setVariantsCount] = useState(30);
 
+    // Обновление компонента
     const causeAnUpdate = () => setForceUpdate(Math.random());
 
+    // Извлечение номера лабораторной работы из заголовка
     const getLabNumberFromTitle = (title) => {
         const match = title.match(/Лабораторная работа №\s*(\d+)/);
         return match ? match[1] : null;
     };
 
+    // Загрузка теории при смене вкладки
     useEffect(() => {
         if (activeTab !== 0 && tabs[activeTab]?.title.startsWith('Лабораторная работа')) {
             const labNumberFromTitle = getLabNumberFromTitle(tabs[activeTab]?.title);
             if (labNumberFromTitle) {
                 setLabNumber(labNumberFromTitle);
-
-                const theoryFilePath = labFiles[labNumberFromTitle]?.theory;
-                if (theoryFilePath) {
-                    fetch(theoryFilePath)
-                        .then((response) => response.text())
-                        .then(setTheoryContent)
-                        .catch((error) => console.error('Ошибка при загрузке теории:', error));
-                } else {
-                    console.error(`Файл теории для лабораторной работы №${labNumberFromTitle} не найден`);
-                }
+                loadTheoryContent(labNumberFromTitle);
             }
         }
     }, [activeTab, tabs]);
 
+    // Загрузка задания при изменении содержимого задания или варианта
     useEffect(() => {
         if (taskContent && taskContentRef.current) {
             showVariant(activeVariant);
         }
-    }, [taskContent, activeVariant, taskContentRef, forceUpdate]);
+    }, [taskContent, activeVariant]);
 
+    // Функция для добавления вкладки
     const addTab = (title, content = '') => {
         const existingTabIndex = tabs.findIndex((tab) => tab.title === title);
         if (existingTabIndex !== -1) {
             setActiveTab(existingTabIndex);
         } else {
-            setTabs([
-                ...tabs,
-                { title, content },
-            ]);
+            setTabs((prevTabs) => [...prevTabs, { title, content }]);
             setActiveTab(tabs.length);
         }
     };
 
+    // Загрузка теории
+    const loadTheoryContent = (labNumberFromTitle) => {
+        const theoryFilePath = labFiles[labNumberFromTitle]?.theory;
+        if (theoryFilePath) {
+            fetch(theoryFilePath)
+                .then((response) => response.text())
+                .then(setTheoryContent)
+                .catch((error) => console.error('Ошибка при загрузке теории:', error));
+        } else {
+            console.error(`Файл теории для лабораторной работы №${labNumberFromTitle} не найден`);
+        }
+    };
+
+    // Загрузка задания
     const loadTaskContent = () => {
         const taskFilePath = labFiles[labNumber]?.tasks?.path;
-        const variantsCount = labFiles[labNumber]?.tasks?.count;
         if (taskFilePath) {
             fetch(taskFilePath)
                 .then((response) => response.text())
@@ -72,6 +78,7 @@ function App() {
         }
     };
 
+    // Отображение выбранного варианта задания
     const showVariant = (variantIndex) => {
         if (taskContentRef.current) {
             const variants = taskContentRef.current.querySelectorAll('.variant');
@@ -83,30 +90,25 @@ function App() {
         }
     };
 
+    // Закрытие других вкладок
     const closeOtherTabs = (index) => {
         setTabs((prevTabs) => prevTabs.slice(0, index + 1));
         setActiveTab(index);
     };
 
+    // Стиль контента для лабораторной работы
     const getContentBoxStyle = (tabTitle) => {
         if (tabTitle.startsWith("Лабораторная работа")) {
             return { width: '1098px', height: '612px' };
         }
     };
 
-    const handleTheoryClick = () => {
-        addTab("Теория", "");
-    };
-
-    const handleExampleClick = () => {
-        addTab("Пример", "");
-    };
-
+    // Обработчики кликов по кнопкам
+    const handleTheoryClick = () => addTab("Теория", "");
+    const handleExampleClick = () => addTab("Пример", "");
     const handleTasksClick = () => {
         const variantsCount = labFiles[labNumber]?.tasks?.count;
-        if (variantsCount) {
-            setVariantsCount(variantsCount);
-        }
+        if (variantsCount) setVariantsCount(variantsCount);
         addTab("Задания", "");
     };
 
@@ -118,25 +120,25 @@ function App() {
         addTab(`Вариант №${variantIndex}`, "");
     };
 
-    return (
-        <div className="App">
-            <div className="background" />
-            <div className="tabs-container">
-                {tabs.map((tab, index) => (
-                    <React.Fragment key={index}>
-                        <div
-                            className={`tab ${activeTab === index ? 'active' : ''}`}
-                            onClick={() => closeOtherTabs(index)}
-                        >
-                            {tab.title}
-                        </div>
-                        {index !== tabs.length - 1 && <div className="arrow">→</div>}
-                    </React.Fragment>
-                ))}
-            </div>
+    // Рендеринг вкладок
+    const renderTabs = () => {
+        return tabs.map((tab, index) => (
+            <React.Fragment key={index}>
+                <div
+                    className={`tab ${activeTab === index ? 'active' : ''}`}
+                    onClick={() => closeOtherTabs(index)}
+                >
+                    {tab.title}
+                </div>
+                {index !== tabs.length - 1 && <div className="arrow">→</div>}
+            </React.Fragment>
+        ));
+    };
 
-            {/* Вкладка "Главная" */}
-            {activeTab === 0 && (
+    // Рендеринг содержимого в зависимости от активной вкладки
+    const renderContent = () => {
+        if (activeTab === 0) {
+            return (
                 <div className="content-box">
                     <div className="tab-content">
                         <div className="main-tab-content">
@@ -160,15 +162,14 @@ function App() {
                         </div>
                     </div>
                 </div>
-            )}
+            );
+        }
 
-            {/* Вкладка "Лабораторная работа" */}
-            {activeTab !== 0 && tabs[activeTab]?.title.startsWith("Лабораторная работа") && (
+        if (tabs[activeTab]?.title.startsWith("Лабораторная работа")) {
+            return (
                 <div className="content-box" style={getContentBoxStyle(tabs[activeTab]?.title)}>
                     <div className="tab-content">
-                        <div className="lab-work-title">
-                            {tabs[activeTab]?.title}
-                        </div>
+                        <div className="lab-work-title">{tabs[activeTab]?.title}</div>
                         <p>{tabs[activeTab]?.content}</p>
                         <div className="lab-buttons-container">
                             <button className="lab-button" onClick={handleTheoryClick}>Теория</button>
@@ -177,10 +178,11 @@ function App() {
                         <button className="lab-button lab-button-variants" onClick={handleTasksClick}>Варианты заданий</button>
                     </div>
                 </div>
-            )}
+            );
+        }
 
-            {/* Вкладка "Задания" */}
-            {activeTab !== 0 && tabs[activeTab]?.title === 'Задания' && (
+        if (tabs[activeTab]?.title === 'Задания') {
+            return (
                 <div className="task-variants-container">
                     <div className="task-buttons">
                         {Array.from({ length: variantsCount }, (_, index) => (
@@ -195,28 +197,39 @@ function App() {
                         ))}
                     </div>
                 </div>
-            )}
+            );
+        }
 
-            {/* Вкладка "Пример" */}
-            {activeTab !== 0 && tabs[activeTab]?.title === 'Пример' && (
-                <div className="accordion-container">
-                    <Accordion labNumber={labNumber} />
-                </div>
-            )}
+        if (tabs[activeTab]?.title === 'Пример') {
+            return <div className="accordion-container"><Accordion labNumber={labNumber} /></div>;
+        }
 
-            {/* Вкладка "Теория" */}
-            {activeTab !== 0 && tabs[activeTab]?.title === 'Теория' && (
+        if (tabs[activeTab]?.title === 'Теория') {
+            return (
                 <div className="theory-container">
                     <div className="theory-content" dangerouslySetInnerHTML={{ __html: theoryContent }} />
                 </div>
-            )}
+            );
+        }
 
-            {/* Вкладка "Вариант" */}
-            {activeTab !== 0 && tabs[activeTab]?.title.startsWith("Вариант №") && (
+        if (tabs[activeTab]?.title.startsWith("Вариант №")) {
+            return (
                 <div className="theory-container">
                     <div ref={taskContentRef} className="theory-content" dangerouslySetInnerHTML={{ __html: taskContent }} />
                 </div>
-            )}
+            );
+        }
+
+        return null;
+    };
+
+    return (
+        <div className="App">
+            <div className="background" />
+            <div className="tabs-container">
+                {renderTabs()}
+            </div>
+            {renderContent()}
         </div>
     );
 }
