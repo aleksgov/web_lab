d42bfd34fafbaff69e937d503d4bab6557250473
aleksgov <aleksandrgovo@gmail.com>
2026-03-06 00:13:47 +0300

    migrate to vite&pnpm

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { labFiles } from '../Globals'; // Импортируем labFiles
+import { labFiles } from '../Globals';
 import '../styles/Accordion.css';
 import SyntaxHighlighter from './SyntaxHighlighter';
 
